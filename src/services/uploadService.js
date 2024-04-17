981c61d735b7ce54c54d4183b06a7cb2f32f819b
marcelocastelari <marcelocastelari@gmail.com>
2024-04-16 22:06:14 -0300

    feat: add processFile function to uploadService

--- src/services/uploadService.js
+++ src/services/uploadService.js
@@ -1,5 +1,32 @@
-module.exports = {
-    processFile(file) { }
+const pino = require('pino');
+const logger = pino();
+
+const readFile = (file)  => {
+    const data = file.buffer.toString('utf-8');
+
+    if(!data) {
+        logger.info(`[uploadService][readFile] File ${file.originalname} is empty.`);
+        throw new Error("File is empty.");
+    }
+
+    const rows = data.split('\n');
+    return rows;
 }
 
-
+module.exports = {
+    processFile(file) {
+        if(!file) {
+            logger.info(`[uploadService][processFile] No file was uploaded.`);
+            throw new Error("No file was uploaded.")
+        }
+    
+        if(file.mimetype !== 'text/plain') {
+            logger.info(`[uploadService][processFile] File ${file.originalname} is not a text file.`);
+            throw new Error("File is not a text file.");
+        }
+    
+        logger.info(`[uploadService][processFile] File uploaded: ${file.originalname}, processing...`);
+        const data = readFile(file);
+        return(`File uploaded ${file.originalname}`);
+    }
+}

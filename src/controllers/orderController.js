bbb323ad92a1b2661e5960098ff729e4b9a66f7f
marcelocastelari <marcelocastelari@gmail.com>
2024-04-17 22:13:17 -0300

    chore: rename files with prefix 'order'

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,10 +1,10 @@
-const uploadService = require('../services/uploadService')
+const orderService = require('../services/orderService')
 
 
 module.exports = {
     async uploadFile(req, res) {
         try {
-            const response = await uploadService.processFile(req.file)
+            const response = await orderService.processFile(req.file)
             res.status(200).json(response)
         } catch (error) {
             res.status(400).send(error.message)

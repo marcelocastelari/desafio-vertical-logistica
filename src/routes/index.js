2b209430b56715cda506e811de062d618e0e225c
marcelocastelari <marcelocastelari@gmail.com>
2024-04-17 22:23:26 -0300

    fix: adjust orderController import on routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const uploadController = require('../controllers/uploadController');
+const orderController = require('../controllers/orderController');
 const router = express.Router();
 const multer = require('multer');
 
 const upload = multer();
 
-router.post('/process-file', upload.single('data'), uploadController.uploadFile)
+router.post('/process-file', upload.single('data'), orderController.uploadFile)
 router.post('/list')
 
 module.exports = router;
